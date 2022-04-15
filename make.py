cc6eec1f3b63f247525a5723a0c7f12f24db1a11
newpaxonian <newpaxonian@gmail.com>
2022-04-15 02:06:29 -0500

    First version. Some problems with structure

--- make.py
+++ make.py
@@ -1,23 +1,66 @@
 import yaml
 import os
 
-def process(entries): #TODO fix recursion over directories
-  for entry in entries:
-    if entry["type"] == "directory":
-      os.makedirs(entry["name"], exist_ok=True) # Thanks @pLumo
-      os.chdir(entry["name"])
-      process(entry.get("contents", []))
-      os.chdir('..')
-    if entry["type"] == "file":
-      with open(entry["name"], "w"): pass
-   #if entry["type"] == "link":
-    #  os.symlink(entry["name"], entry["target"])
+def make_directory(entries): #TODO fix recursion over directories
+  # for entry in entries:
+  #   if isinstance(entries,dict):
+  #     if entry == 'name':
+  #       os.makedirs(entry['name'], exist_ok=True)
+  #       os.chdir(entry['name'])
+  #     elif entry == 'contents':
+  #       make_directory(entries['contents'])
+  #       os.chdir('..')
+  #     if entry["type"] == "file":
+  #       with open(entry["name"], "w"): pass
+  #     #if entry["type"] == "link":
+  #     #  os.symlink(entry["name"], entry["target"])
+  if isinstance(entries, dict):
+    type_file = None
+    type_directory = None
+    for key in entries:
+      if key == 'contents':
+        make_directory(entries[key])
+      elif key == 'type' and entries[key] == 'directory':
+        type_directory = True
+        type_file == False
+      elif key == 'type' and entries[key] == 'file':
+        type_directory = False
+        type_file = True
+      elif type_directory == True and key == 'name':
+        os.makedirs(entries[key], exist_ok=True)
+        os.chdir(entries[key])
+        os.chdir('..')
+        type_directory = False
+      elif type_file == True and key == 'name':
+        with open(entries['name'], 'w'):
+          pass
+        type_file = False
+  elif isinstance(entries,list) and len(entries)>0:
+    for key in entries:
+      make_directory(key)
 
 if __name__ == '__main__':
   with open("structure.yaml", "r") as f:
     structure = yaml.load(f, Loader=yaml.FullLoader)
 
-    process(structure['structure'][0])
+    make_directory(structure['structure'])
 
 
+
+  # d = {'a':1,'b':2,'c':[{'d':4,'f':6,'c':[{'g':8},{'h':9},{'i':10}]},{'e':5}]}
+
+  # def func(s):
+  #   if isinstance(s,dict):
+  #     for i in s:
+  #       print(i)
+  #       if i == 'c':
+  #         func(s['c'])
+  #   elif isinstance(s,list):
+  #     for i in s:
+  #       print(i,type(i))
+  #       func(i)
   
+  # func(d)
+
+
+
