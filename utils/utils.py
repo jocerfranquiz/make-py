16fc18298ad0245507b53bbc979d9c343c728dbe
newpaxonian <newpaxonian@gmail.com>
2022-04-16 22:41:57 -0500

    adding folder for utils module

---
+++ utils/utils.py
@@ -0,0 +1,6 @@
+# TODO Create a function for traverse the whole directory, with an argument to pass a function
+# TODO Create another function to calculate the sha256 of files and directories for testing
+# TODO Use the traverse function to calculate all the sha256.
+# Do I need to add those sha in another structure or in the YAML file?
+# TODO replace in make.py the function for create the ditedtory with the new one here
+# TODO create the test function
