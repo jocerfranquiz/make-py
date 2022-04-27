3f5384d99a43f159e9165f42bc66cac8b4012f4c
newpaxonian <newpaxonian@gmail.com>
2022-04-27 00:05:53 -0500

    some aesthetic modifications to make.py

--- src/app/make.py
+++ src/app/make.py
@@ -2,15 +2,19 @@ import yaml
 import os
 import utils
 
+# TODO move this to a config file
+PROJECT_PATH = '.'
+
 # TODO add log register
-def transform(x):
+def transform(x, path = PROJECT_PATH):
   '''Apply a function to objrcts in the structure
   Args:
       x (dict): Dictionary with a valid project structure
+      path (str): path to the project root
   '''
   if x['type'] == 'directory':
-    print(f"directory: {x['name']}")
-    utils.create_directory(x['name'])
+    print(f'directory: {x["name"]}')
+    utils.create_directory(x['name'],path)
 
     if len(x['contents']) > 0:
       os.chdir(x['name'])
@@ -18,13 +22,13 @@ def transform(x):
       os.chdir('..')
 
   if x['type'] == 'file':
-    print(f"file: {x['name']}")
+    print(f'file: {x["name"]}')
     utils.create_file(x['name'])
 
 if __name__ == '__main__':
-  with open("structure.yml", "r") as f:
-    structure = yaml.load(f, Loader=yaml.FullLoader)
-    utils.traverse(structure['structure'],transform)
+  with open('structure.yml', 'r') as f:
+    structure = yaml.load(f, Loader = yaml.FullLoader)
+    utils.traverse(structure['structure'], transform)
 
 
 
